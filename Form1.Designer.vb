625b9e7d8f1866cc77d7e221bbbbe982b3a693e6
David Gabriel Bermudez <daveqacode@users.noreply.github.com>
2020-03-14 23:32:45 +0800

    7 segment

---
+++ Form1.Designer.vb
@@ -0,0 +1,363 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox25 = New System.Windows.Forms.TextBox()
+        Me.TextBox24 = New System.Windows.Forms.TextBox()
+        Me.TextBox23 = New System.Windows.Forms.TextBox()
+        Me.TextBox22 = New System.Windows.Forms.TextBox()
+        Me.TextBox21 = New System.Windows.Forms.TextBox()
+        Me.TextBox20 = New System.Windows.Forms.TextBox()
+        Me.TextBox19 = New System.Windows.Forms.TextBox()
+        Me.TextBox18 = New System.Windows.Forms.TextBox()
+        Me.TextBox17 = New System.Windows.Forms.TextBox()
+        Me.TextBox16 = New System.Windows.Forms.TextBox()
+        Me.TextBox15 = New System.Windows.Forms.TextBox()
+        Me.TextBox14 = New System.Windows.Forms.TextBox()
+        Me.TextBox13 = New System.Windows.Forms.TextBox()
+        Me.TextBox12 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.TextBox25)
+        Me.Panel1.Controls.Add(Me.TextBox24)
+        Me.Panel1.Controls.Add(Me.TextBox23)
+        Me.Panel1.Controls.Add(Me.TextBox22)
+        Me.Panel1.Controls.Add(Me.TextBox21)
+        Me.Panel1.Controls.Add(Me.TextBox20)
+        Me.Panel1.Controls.Add(Me.TextBox19)
+        Me.Panel1.Controls.Add(Me.TextBox18)
+        Me.Panel1.Controls.Add(Me.TextBox17)
+        Me.Panel1.Controls.Add(Me.TextBox16)
+        Me.Panel1.Controls.Add(Me.TextBox15)
+        Me.Panel1.Controls.Add(Me.TextBox14)
+        Me.Panel1.Controls.Add(Me.TextBox13)
+        Me.Panel1.Controls.Add(Me.TextBox12)
+        Me.Panel1.Controls.Add(Me.TextBox11)
+        Me.Panel1.Controls.Add(Me.TextBox10)
+        Me.Panel1.Controls.Add(Me.TextBox9)
+        Me.Panel1.Controls.Add(Me.TextBox8)
+        Me.Panel1.Controls.Add(Me.TextBox7)
+        Me.Panel1.Controls.Add(Me.TextBox6)
+        Me.Panel1.Controls.Add(Me.TextBox5)
+        Me.Panel1.Controls.Add(Me.TextBox4)
+        Me.Panel1.Controls.Add(Me.TextBox3)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(256, 254)
+        Me.Panel1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(10, 10)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox1.TabIndex = 25
+        '
+        'TextBox25
+        '
+        Me.TextBox25.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox25.Location = New System.Drawing.Point(202, 202)
+        Me.TextBox25.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox25.Name = "TextBox25"
+        Me.TextBox25.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox25.TabIndex = 49
+        '
+        'TextBox24
+        '
+        Me.TextBox24.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox24.Location = New System.Drawing.Point(154, 202)
+        Me.TextBox24.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox24.Name = "TextBox24"
+        Me.TextBox24.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox24.TabIndex = 48
+        '
+        'TextBox23
+        '
+        Me.TextBox23.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox23.Location = New System.Drawing.Point(106, 202)
+        Me.TextBox23.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox23.Name = "TextBox23"
+        Me.TextBox23.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox23.TabIndex = 47
+        '
+        'TextBox22
+        '
+        Me.TextBox22.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox22.Location = New System.Drawing.Point(58, 202)
+        Me.TextBox22.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox22.Name = "TextBox22"
+        Me.TextBox22.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox22.TabIndex = 46
+        '
+        'TextBox21
+        '
+        Me.TextBox21.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox21.Location = New System.Drawing.Point(10, 202)
+        Me.TextBox21.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox21.Name = "TextBox21"
+        Me.TextBox21.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox21.TabIndex = 45
+        '
+        'TextBox20
+        '
+        Me.TextBox20.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox20.Location = New System.Drawing.Point(202, 154)
+        Me.TextBox20.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox20.Name = "TextBox20"
+        Me.TextBox20.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox20.TabIndex = 44
+        '
+        'TextBox19
+        '
+        Me.TextBox19.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox19.Location = New System.Drawing.Point(154, 154)
+        Me.TextBox19.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox19.Name = "TextBox19"
+        Me.TextBox19.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox19.TabIndex = 43
+        '
+        'TextBox18
+        '
+        Me.TextBox18.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox18.Location = New System.Drawing.Point(106, 154)
+        Me.TextBox18.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox18.Name = "TextBox18"
+        Me.TextBox18.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox18.TabIndex = 42
+        '
+        'TextBox17
+        '
+        Me.TextBox17.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox17.Location = New System.Drawing.Point(58, 154)
+        Me.TextBox17.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox17.Name = "TextBox17"
+        Me.TextBox17.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox17.TabIndex = 41
+        '
+        'TextBox16
+        '
+        Me.TextBox16.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox16.Location = New System.Drawing.Point(10, 154)
+        Me.TextBox16.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox16.Name = "TextBox16"
+        Me.TextBox16.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox16.TabIndex = 40
+        '
+        'TextBox15
+        '
+        Me.TextBox15.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox15.Location = New System.Drawing.Point(202, 106)
+        Me.TextBox15.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox15.Name = "TextBox15"
+        Me.TextBox15.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox15.TabIndex = 39
+        '
+        'TextBox14
+        '
+        Me.TextBox14.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox14.Location = New System.Drawing.Point(154, 106)
+        Me.TextBox14.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox14.Name = "TextBox14"
+        Me.TextBox14.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox14.TabIndex = 38
+        '
+        'TextBox13
+        '
+        Me.TextBox13.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox13.Location = New System.Drawing.Point(106, 106)
+        Me.TextBox13.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox13.Name = "TextBox13"
+        Me.TextBox13.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox13.TabIndex = 37
+        '
+        'TextBox12
+        '
+        Me.TextBox12.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox12.Location = New System.Drawing.Point(58, 106)
+        Me.TextBox12.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox12.Name = "TextBox12"
+        Me.TextBox12.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox12.TabIndex = 36
+        '
+        'TextBox11
+        '
+        Me.TextBox11.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox11.Location = New System.Drawing.Point(10, 106)
+        Me.TextBox11.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox11.TabIndex = 35
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox10.Location = New System.Drawing.Point(202, 58)
+        Me.TextBox10.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox10.TabIndex = 34
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox9.Location = New System.Drawing.Point(154, 58)
+        Me.TextBox9.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox9.TabIndex = 33
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox8.Location = New System.Drawing.Point(106, 58)
+        Me.TextBox8.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox8.TabIndex = 32
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox7.Location = New System.Drawing.Point(58, 58)
+        Me.TextBox7.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox7.TabIndex = 31
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox6.Location = New System.Drawing.Point(10, 58)
+        Me.TextBox6.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox6.TabIndex = 30
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox5.Location = New System.Drawing.Point(202, 10)
+        Me.TextBox5.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox5.TabIndex = 29
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(154, 10)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox4.TabIndex = 28
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(106, 10)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox3.TabIndex = 27
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(58, 10)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(44, 44)
+        Me.TextBox2.TabIndex = 26
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(254, 252)
+        Me.Controls.Add(Me.Panel1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox25 As TextBox
+    Friend WithEvents TextBox24 As TextBox
+    Friend WithEvents TextBox23 As TextBox
+    Friend WithEvents TextBox22 As TextBox
+    Friend WithEvents TextBox21 As TextBox
+    Friend WithEvents TextBox20 As TextBox
+    Friend WithEvents TextBox19 As TextBox
+    Friend WithEvents TextBox18 As TextBox
+    Friend WithEvents TextBox17 As TextBox
+    Friend WithEvents TextBox16 As TextBox
+    Friend WithEvents TextBox15 As TextBox
+    Friend WithEvents TextBox14 As TextBox
+    Friend WithEvents TextBox13 As TextBox
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,153 @@
+﻿Public Class Form1
+
+    Private Sub wait(ByVal interval As Integer)
+        Dim sw As New Stopwatch
+        sw.Start()
+        Do While sw.ElapsedMilliseconds < interval
+
+            Application.DoEvents()
+        Loop
+        sw.Stop()
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+        Dim index As Byte = 0
+
+        While index < 1
+            '0'
+
+            TextBox1.BackColor = Color.Red
+            TextBox2.BackColor = Color.Red
+            TextBox3.BackColor = Color.Red
+            TextBox4.BackColor = Color.Red
+            TextBox5.BackColor = Color.Red
+            TextBox6.BackColor = Color.Red
+            TextBox7.BackColor = Color.Black
+            TextBox8.BackColor = Color.Black
+            TextBox9.BackColor = Color.Black
+            TextBox10.BackColor = Color.Red
+            TextBox11.BackColor = Color.Red
+            TextBox12.BackColor = Color.Black
+            TextBox13.BackColor = Color.Black
+            TextBox14.BackColor = Color.Black
+            TextBox15.BackColor = Color.Red
+            TextBox16.BackColor = Color.Red
+            TextBox17.BackColor = Color.Black
+            TextBox18.BackColor = Color.Black
+            TextBox19.BackColor = Color.Black
+            TextBox20.BackColor = Color.Red
+            TextBox21.BackColor = Color.Red
+            TextBox22.BackColor = Color.Red
+            TextBox23.BackColor = Color.Red
+            TextBox24.BackColor = Color.Red
+            TextBox25.BackColor = Color.Red
+
+            '1'
+            wait(1000)
+            TextBox1.BackColor = Color.Black
+            TextBox2.BackColor = Color.Black
+            TextBox3.BackColor = Color.Black
+            TextBox4.BackColor = Color.Black
+            TextBox6.BackColor = Color.Black
+            TextBox11.BackColor = Color.Black
+            TextBox16.BackColor = Color.Black
+            TextBox21.BackColor = Color.Black
+            TextBox22.BackColor = Color.Black
+            TextBox23.BackColor = Color.Black
+            TextBox24.BackColor = Color.Black
+
+            '2'
+            wait(1000)
+            TextBox1.BackColor = Color.Red
+            TextBox2.BackColor = Color.Red
+            TextBox3.BackColor = Color.Red
+            TextBox4.BackColor = Color.Red
+            TextBox6.BackColor = Color.Black
+            TextBox11.BackColor = Color.Red
+            TextBox12.BackColor = Color.Red
+            TextBox13.BackColor = Color.Red
+            TextBox14.BackColor = Color.Red
+            TextBox16.BackColor = Color.Red
+            TextBox20.BackColor = Color.Black
+            TextBox21.BackColor = Color.Red
+            TextBox22.BackColor = Color.Red
+            TextBox23.BackColor = Color.Red
+            TextBox24.BackColor = Color.Red
+            TextBox25.BackColor = Color.Red
+
+            '3'
+            wait(1000)
+
+            TextBox16.BackColor = Color.Black
+            TextBox20.BackColor = Color.Red
+
+            '4'
+            wait(1000)
+            TextBox2.BackColor = Color.Black
+            TextBox3.BackColor = Color.Black
+            TextBox4.BackColor = Color.Black
+            TextBox6.BackColor = Color.Red
+            TextBox11.BackColor = Color.Red
+            TextBox12.BackColor = Color.Red
+            TextBox13.BackColor = Color.Red
+            TextBox14.BackColor = Color.Red
+            TextBox21.BackColor = Color.Black
+            TextBox22.BackColor = Color.Black
+            TextBox23.BackColor = Color.Black
+            TextBox24.BackColor = Color.Black
+
+            '5'
+            wait(1000)
+            TextBox2.BackColor = Color.Red
+            TextBox3.BackColor = Color.Red
+            TextBox4.BackColor = Color.Red
+            TextBox10.BackColor = Color.Black
+            TextBox21.BackColor = Color.Red
+            TextBox22.BackColor = Color.Red
+            TextBox23.BackColor = Color.Red
+            TextBox24.BackColor = Color.Red
+
+            '6'
+            wait(1000)
+            TextBox16.BackColor = Color.Red
+
+            '7'
+            wait(1000)
+            TextBox6.BackColor = Color.Black
+            TextBox10.BackColor = Color.Red
+            TextBox11.BackColor = Color.Black
+            TextBox12.BackColor = Color.Black
+            TextBox13.BackColor = Color.Black
+            TextBox14.BackColor = Color.Black
+            TextBox16.BackColor = Color.Black
+            TextBox21.BackColor = Color.Black
+            TextBox22.BackColor = Color.Black
+            TextBox23.BackColor = Color.Black
+            TextBox24.BackColor = Color.Black
+
+            '8'
+            wait(1000)
+            TextBox6.BackColor = Color.Red
+            TextBox11.BackColor = Color.Red
+            TextBox12.BackColor = Color.Red
+            TextBox13.BackColor = Color.Red
+            TextBox14.BackColor = Color.Red
+            TextBox16.BackColor = Color.Red
+            TextBox21.BackColor = Color.Red
+            TextBox22.BackColor = Color.Red
+            TextBox23.BackColor = Color.Red
+            TextBox24.BackColor = Color.Red
+
+            '9'
+            wait(1000)
+            TextBox16.BackColor = Color.Black
+
+
+            wait(1000)
+        End While
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
+
+    End Sub
+End Class
